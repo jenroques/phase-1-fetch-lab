b8c1dc8b834dfa7dc2fa104a6a41c38f3af23681
Jennifer <jenniferroques@gmail.com>
2021-12-12 13:42:48 -0600

    commit tree 41827c5d8f5de8314d0b3e58d09951a84d9f1a13

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -8,10 +10,10 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
-    main.appendChild(h2);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+
 });
+
